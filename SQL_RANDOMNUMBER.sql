b4bfe6f6c03b6f6eae9ccb5e09e2c8d9da070706
Chris Huntley <chuntley@blackshellconsulting.com>
2017-03-31 02:32:23 -0500

    Troubleshooting update

--- SQL_RANDOMNUMBER.sql
+++ SQL_RANDOMNUMBER.sql
@@ -22,8 +22,19 @@ abs(rownumber) AS absolutevalue, -- just changes a negative number into a positi
 checksum(rownumber) AS checksumofvalue,
 NewID() AS newid_check,
 (Checksum(NewID())) AS checksumofnewID,
-ABS((Checksum(NewID())))  AS checksumofABS
+ABS((Checksum(NewID())))  AS checksumofABS,
+ABS(Checksum(NewID()) % 1)+1 AS q1 ,
+ABS(Checksum(NewID()) % 2)+1  AS q2 ,
+ABS(Checksum(NewID()) % 3)+1  AS q3 ,
+ABS(Checksum(NewID()) % 4)+1 AS q4 ,
+ABS(Checksum(NewID()) % 5)+1  AS q5,
+ABS(Checksum(NewID()) % 6)+1  AS q6,
+ABS(Checksum(NewID()) % 7)+1  AS q7,
+ABS(Checksum(NewID()) % 8)+1 AS q8 ,
+ABS(Checksum(NewID()) % 9)+1 as q9 ,
+ABS(Checksum(NewID()) % 10)+1 AS q10  ,
+ABS(Checksum(NewID()) % 19) +1 AS q11,
+ABS(Checksum(NewID()) % 17) +1 AS q11
 
-
- FROM @teststore t  
+FROM @teststore t  
 --ABS(Checksum(NewID()) % 6) + 1
